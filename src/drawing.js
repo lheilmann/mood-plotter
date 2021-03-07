cc64f747398216745e4aeabdbebcb3165d19c192
Lukas Heilmann <lukas@familieheilmann.de>
2021-03-07 20:45:49 +0100

    Replace grid pattern with shape pattern

--- src/drawing.js
+++ src/drawing.js
@@ -1,26 +1,26 @@
 const canvasWidth = 360;
 const canvasHeight = canvasWidth;
 
+// NOTE: `valence` and `arousal` are available as global variables here.
+
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent("sketch");
 
-  // Prevent bezier curves from being filled
-  noFill();
-
-  drawGridPattern(valence, arousal);
-  drawCurvePattern(valence, arousal);
+  // drawGridPattern();
+  drawCurvePattern();
+  drawShapePattern();
 }
 
-function draw() {
-  // 60 times per second
-}
+function draw() {}
 
 // ----------
-// Draw
+// Patterns
 // ----------
 
-function drawCurvePattern(valence, arousal) {
+function drawCurvePattern() {
+  let numberOfCurves;
+
   const themeColors = {
     primary: color(random(255), random(255), random(255), 120),
     secondary: color(random(255), random(255), random(255), 255),
@@ -38,11 +38,92 @@ function drawCurvePattern(valence, arousal) {
 
   // Draw curve 10 times
   for (let i = 0; i < 10; i++) {
-    drawCurve();
+    drawRandomCurve();
   }
 }
 
-function drawVertex() {
+function drawShapePattern() {
+  const themeColors = {
+    primary: color(random(255), random(255), random(255), 120),
+    secondary: color(random(255), random(255), random(255), 255),
+    tertiary: color(random(255), random(255), random(255)),
+  };
+
+  // Parameters
+  const numberOfShapes = chooseNumberOfShapes();
+  const shapeType = chooseShapeType();
+
+  stroke(themeColors.primary);
+  drawingContext.shadowOffsetX = 3;
+  drawingContext.shadowOffsetY = 3;
+  drawingContext.shadowBlur = 5;
+  drawingContext.shadowColor = color(themeColors.secondary);
+
+  for (let i = 0; i < numberOfShapes; i++) {
+    const anchorPoint = getRandomPoint();
+
+    if (shapeType === "circle") {
+      ellipse(anchorPoint.x, anchorPoint.y, 50, 50);
+      continue;
+    }
+
+    if (shapeType === "rounded rect") {
+      rect(anchorPoint.x, anchorPoint.y, 50, 50, 20);
+      continue;
+    }
+
+    if (shapeType === "rect") {
+      rect(anchorPoint.x, anchorPoint.y, 50, 50);
+      continue;
+    }
+
+    if (shapeType === "triangle") {
+      triangle(
+        anchorPoint.x,
+        anchorPoint.y,
+        anchorPoint.x + 4,
+        anchorPoint.y,
+        anchorPoint.x + 2,
+        anchorPoint.y + 2
+      );
+      continue;
+    }
+
+    if (shapeType === "line") {
+      const targetPoint = getRandomPoint();
+      line(anchorPoint.x, anchorPoint.y, targetPoint.x, targetPoint.y);
+      continue;
+    }
+  }
+}
+
+// ----------
+// Generation
+// ----------
+
+function chooseNumberOfShapes() {
+  return arousal;
+}
+
+function chooseShapeType() {
+  if (arousal < 50) {
+    return "circle";
+  } else if (arousal < 80) {
+    return "rounded rect";
+  } else if (arousal < 80) {
+    return "rect";
+  } else if (arousal < 90) {
+    return "triangle";
+  } else {
+    return "line";
+  }
+}
+
+// ----------
+// Helpers
+// ----------
+
+function drawRandomVertex() {
   p1 = getRandomPoint();
   p2 = getRandomPoint();
   p3 = getRandomPoint();
@@ -51,7 +132,9 @@ function drawVertex() {
   return bezierVertex(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y);
 }
 
-function drawCurve() {
+function drawRandomCurve() {
+  noFill();
+
   p1 = getRandomPoint();
   p2 = getRandomPoint();
   p3 = getRandomPoint();
@@ -60,68 +143,18 @@ function drawCurve() {
   bezier(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y);
 }
 
-function drawGridPattern(valence, arousal) {
-  const themeColors = {
-    primary: color(random(255), random(255), random(255), 120),
-    secondary: color(random(255), random(255), random(255), 255),
-    tertiary: color(random(255), random(255), random(255)),
-  };
-
-  strokeWeight(10);
-  stroke(themeColors.primary);
-
-  // Shadow configuration
-  drawingContext.shadowOffsetX = 3;
-  drawingContext.shadowOffsetY = 3;
-  drawingContext.shadowBlur = 5;
-  drawingContext.shadowColor = color(themeColors.secondary);
-
-  let grid = get2DGrid();
-  grid.map((column) =>
-    column.map((point) =>
-      triangle(point.x, point.y, point.x + 4, point.y, point.x + 2, point.y + 2)
-    )
-  );
-}
-
 // ----------
 // Utils
 // ----------
 
 function getRandomPoint() {
   return {
-    x: random(0, canvasWidth),
-    y: random(0, canvasHeight),
+    x: random(canvasWidth),
+    y: random(canvasHeight),
   };
 }
 
 function getRandomPoints(n) {
   let points = new Array(n);
-
   return points.map((p) => getRandomPoint());
-}
-
-function get2DGrid() {
-  const offsetX = 56;
-  const offsetY = 31;
-
-  const distanceX = 43;
-  const distanceY = 27;
-
-  const lengthX = Math.floor((canvasWidth - offsetX) / distanceX);
-  const lengthY = Math.floor((canvasHeight - offsetY) / distanceY);
-
-  const grid = new Array(lengthX);
-
-  for (let i = 0; i < lengthX; i++) {
-    grid[i] = new Array(lengthY);
-    for (let j = 0; j < lengthY; j++) {
-      grid[i][j] = {
-        x: offsetX + i * distanceX,
-        y: offsetY + j * distanceY,
-      };
-    }
-  }
-
-  return grid;
 }
